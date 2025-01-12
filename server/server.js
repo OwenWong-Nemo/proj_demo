57fd368d5e631d420d2467717820d63bd134223c
OwenWong-Nemo <owenwong2003@gmail.com>
2025-01-12 12:41:13 +0800

    Modified app config, now up and running. Adding function to server, allowing getting a specific district's weather data

--- server/server.js
+++ server/server.js
@@ -20,7 +20,7 @@ APP.listen(PORT, () => {
     console.log(`Server listening at http://localhost:${PORT}`);
 });
 
-// Asynchronous function to fetch weather
+// Fetch current weather, all 18 Hong Kong district
 async function fetchWeather() {
     try {
         const response = await fetch(API.weather);
@@ -31,5 +31,46 @@ async function fetchWeather() {
     }
 }
 
-// Call the function to perform the fetch operation
-fetchWeather();
+const CURR_LOCATION = "Kowloon City"; // Hard-code, preferably track current location
+async function fetchLocalWeatherData(district) {
+    // Declaring variable
+    let curr_loc_weather;
+    let rainfall, uv, temperature;
+
+    try {
+        const response = await fetch(API.weather);
+        const curr_weather = await response.json();
+
+        // Get rainfall
+        for (let index in curr_weather.rainfall.data) {
+            let record = curr_weather.rainfall.data[index];
+            if (record.place === district) {
+                rainfall = record;
+                break; // Exit loop once the required place is found
+            }
+        }
+
+        uv = curr_weather.uvindex.data;
+
+        // Get temperature
+        for (let index in curr_weather.temperature.data) {
+            let record = curr_weather.temperature.data[index];
+            if (record.place === district) {
+                temperature = record;
+                break; // Exit loop once the required place is found
+            }
+        }
+    } catch (error) {
+        console.error('Failed to fetch weather:', error);
+    };
+
+    curr_loc_weather = {
+        "rainfall": rainfall,
+        "uv": uv,
+        "temperature": temperature
+    };
+    console.log(curr_loc_weather);
+    return curr_loc_weather;
+}
+
+fetchLocalWeatherData(CURR_LOCATION);
